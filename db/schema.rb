7c2b0585a0436e9e6fdfd76eac1575fce80d48de
gtsofa <jlsnyule@yahoo.com>
2019-11-05 00:49:35 +0300

    add crud operations A user can create a post A user can view a post A usr can edit a post A user can delete a post

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_105600) do
+ActiveRecord::Schema.define(version: 2019_11_04_212442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "text"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
